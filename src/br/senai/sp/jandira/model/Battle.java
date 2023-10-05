e37058c938a387d6001663596a1efd04c8e3b3a6
VITOR SANTANA DE JESUS <sn1079379@cfp127aluno.local>
2023-10-05 15:23:58 -0300

    Game

--- src/br/senai/sp/jandira/model/Battle.java
+++ src/br/senai/sp/jandira/model/Battle.java
@@ -8,20 +8,20 @@ public class Battle {
 
     Scanner scanner = new Scanner(System.in);
 
-    public void startBattle(Player player, Enemy enemy){
+    public void startBattle(Player player, Enemy enemy) {
 
         boolean continuar = true;
 
-        while (true){
+        while (true) {
 
             int lifePlayer = player.getVida();
             int lifeEnemy = enemy.getVida();
 
-            if (lifeEnemy <= 0){
+            if (lifeEnemy <= 0) {
                 System.out.println("O player " + player.getNome() + " é o Vencedor !!!");
                 break;
 
-            } else if (lifePlayer <= 0){
+            } else if (lifePlayer <= 0) {
                 System.out.println("O enemy " + enemy.getNome() + " é o Vencedor !!!");
                 break;
             }
@@ -34,22 +34,64 @@ public class Battle {
 
             String attack = scanner.nextLine();
 
-            if (attack.equalsIgnoreCase("V")){
+            if (attack.equalsIgnoreCase("V")) {
                 System.out.println("/--------  O Player Atacou  --------/");
 
-                int danoPlayer = ((int) (Math.random()*20) + 1);
+                int danoPlayer = ((int) (Math.random() * 20) + 1);
 
-                enemy.subtrairVida(danoPlayer);
-                System.out.println("O dano gerado foi de: " + danoPlayer);
-                System.out.println("/----------------------------------/");
+                int chanceDefesa = ((int) (Math.random() * 2) + 1);
+
+                System.out.println("Informe um numero para tentar defesa [1-5]");
+                int userDefesa = scanner.nextInt();
+                scanner.nextLine();
+
+                if (chanceDefesa == userDefesa) {
+                    int valorDefesa = ((int) (Math.random() * 20) + 1);
+
+                    if (valorDefesa > danoPlayer) {
+                        System.out.println("A defesa bloqueou 100%");
+
+                    } else {
+                        danoPlayer -= valorDefesa;
+                        enemy.subtrairVida(danoPlayer);
+                    }
+
+                } else {
+                    System.out.println("Errou a Defesa !!!");
+                    enemy.subtrairVida(danoPlayer);
+                    System.out.println("O dano gerado foi de: " + danoPlayer);
+                    System.out.println("/----------------------------------/");
+                }
 
             } else if (attack.equalsIgnoreCase("A")) {
                 System.out.println("/---------- O Enemy Atacou ---------/");
 
-                int danoEnemy = (int) (Math.random()*20 + 1);
-                player.subtrairVida(danoEnemy);
-                System.out.println("O dano gerado foi de: " + danoEnemy);
-                System.out.println("/-----------------------------------/");
+                int danoEnemy = (int) (Math.random() * 20 + 1);
+
+
+                int chanceDefesaPlayer = ((int) (Math.random() * 5) + 1);
+
+                System.out.println("Informe um numero para tentar defesa [1-5]");
+                int playerDefesa = scanner.nextInt();
+                scanner.nextLine();
+
+                if (chanceDefesaPlayer == playerDefesa) {
+                    int valorDefesaPlayer = ((int) (Math.random() * 20) + 1);
+
+                    if (valorDefesaPlayer > danoEnemy) {
+                        System.out.println("A defesa bloqueou 100%");
+
+                    } else {
+                        danoEnemy -= valorDefesaPlayer;
+                        player.subtrairVida(danoEnemy);
+                    }
+
+                } else {
+                    System.out.println("Errou o Bloqueio !!");
+                    player.subtrairVida(danoEnemy);
+                    System.out.println("O dano gerado foi de: " + danoEnemy);
+                    System.out.println("/-----------------------------------/");
+                }
 
             } else {
                 System.out.println("Digite uma tecla válida !!!");
